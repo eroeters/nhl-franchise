1ed1c6a75685784f5949c8937a41e09bf3676157
Eric Roeters <erroeters@gmail.com>
2022-10-10 15:43:39 -0400

    added express and node modules

---
+++ templates/top-navbar/script.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.send("hi")
+})
+
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+})
