820f9ae7f06ecc9617f4e0bdfd787926d6b190e9
Eric Ubuntu <erroeters@gmail.com>
2023-09-21 16:58:29 -0400

    updated file format and started work on roster page

---
+++ new-franchise-tracker/Roster Page/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    let x = document.getElementById("testDrop");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
